9fcea2c8947a14c3f5c4e083a5d63b590c80f263
Hawli <126889064+hawl1@users.noreply.github.com>
2024-01-26 13:30:14 +0000

    rewrite

--- go.mod
+++ go.mod
@@ -1,14 +1,15 @@
 module lunar-hill.xyz/renderer
 
-go 1.21.4
+go 1.21.6
 
 require (
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/aws/aws-lambda-go v1.45.0
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/hawl1/brickgl v0.0.0-20240122061428-3554bc992e2a
 )
 
 require (
 	github.com/disintegration/imaging v1.6.2 // indirect
-	github.com/fogleman/fauxgl v0.0.0-20200818143847-27cddc103802 // indirect
 	github.com/fogleman/simplify v0.0.0-20170216171241-d32f302d5046 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 )
--- main.go
+++ main.go
@@ -1,348 +1,114 @@
 package main
 
 import (
-	. "github.com/fogleman/fauxgl"
-	"github.com/nfnt/resize"
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"image/png"
 	"os"
+	"os/exec"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gofrs/uuid"
+	"github.com/hawl1/brickgl"
 )
 
 const (
-	scale  = 4   // optional supersampling
-	width  = 512 // output width in pixels
-	height = 512 // output height in pixels
-	fovy   = 74  // vertical field of view in degrees
-	near   = 1   // near clipping plane
-	far    = 100 // far clipping plane
+	scale = 3
+	fovy  = 50
+	near  = 0.1
+	far   = 1000
 )
 
 var (
-	//eye    = V(2.57161, 5.40692, 5.3927) // camera position
-	//eye    = V(0, 0, 7)
-	center = V(0, 2.83929, 0) // view center position
-	//up     = V(0, 1, 0) // up vector
-	eye = V(1.5, 4.2, 4)
-	//center = V(0, 3.5, 0)
-	up = V(0, 1, 0)
-	//light  = V(-4.09796, -3.55779, 6.14444).Normalize() // light direction
-	//light  = V(0.1, 0.45, 0.75).Normalize()
-	light = V(0.4, 0.7, 1.1).Normalize()
-	//light  = V(0.75, 0.25, -0.5).Normalize()
-	color = HexColor("#468966") // object color
+	eye    = fauxgl.V(-0.75, 0.85, -2)
+	center = fauxgl.V(0, 0.06, 0)
+	up     = fauxgl.V(0, 1, 0)
+	light  = fauxgl.V(0, 6, -4).Normalize()
+
+	def = "{\"user_id\":13,\"items\":{\"face\":0,\"hats\":[20121,0,0,0,0],\"head\":0,\"tool\":0,\"pants\":0,\"shirt\":0,\"figure\":0,\"tshirt\":0},\"colors\":{\"head\":\"eab372\",\"torso\":\"85ad00\",\"left_arm\":\"eab372\",\"left_leg\":\"37302c\",\"right_arm\":\"eab372\",\"right_leg\":\"37302c\"}}"
 )
 
+// RenderEvent input data to lambda to return an ImageResponse
+type RenderEvent struct {
+	AvatarJSON string
+	Size       int
+}
+
+// ImageResponse lambda response for a base64 encoded render
+type ImageResponse struct {
+	UUID  string
+	Image string
+}
+
 func main() {
-	// load a mesh
-	//if err != nil {
-	//	panic(err)
-	//}
-	//mesh.BiUnitCube()
-	//mesh.SmoothNormalsThreshold(Radians(30))
-	//get vars
-	headcolor := os.Args[1]
-	torsocolor := os.Args[2]
-	leftarmcolor := os.Args[3]
-	rightarmcolor := os.Args[4]
-	leftlegcolor := os.Args[5]
-	rightlegcolor := os.Args[6]
-	faceid := os.Args[7]
-	headid := os.Args[8]
-	tshirtid := os.Args[9]
-	shirtid := os.Args[11]
+	lambda.Start(HandleRenderEvent)
+	/*fmt.Println(HandleRenderEvent(RenderEvent{
+		AvatarJSON: "",
+		Size:       512,
+	}))*/
+}
 
-	allmeshes := make([]*Mesh, 12)
-
-	//create avatar
-
-	mesh := NewEmptyMesh()
-
-	context := NewContext(width*scale, height*scale)
-	//context.ClearColorBufferWith(HexColor("#FFF8E3"))
-
-	//LookAtDirection(forward, up Vector)
-	aspect := float64(width) / float64(height)
-	matrix := LookAt(eye, center, up).Perspective(fovy, aspect, near, far)
-
-	shader := NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = color
-	context.Shader = shader
-	context.DrawMesh(mesh)
-
-	headmesh := NewEmptyMesh()
-	//load head
-	if headid == "0" {
-		headmesh, _ = LoadOBJ("Head.obj")
-	} else {
-		headmesh, _ = LoadOBJ("/var/www/html/itemcache/" + headid + ".obj")
+// HandleRenderEvent function to process the rendering
+func HandleRenderEvent(e RenderEvent) (resp ImageResponse, err error) {
+	if e.AvatarJSON == "" {
+		e.AvatarJSON = def
 	}
-	allmeshes[1] = mesh
-	mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	shader = NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = HexColor(headcolor)
-	//HexColor("#FEC400")
-	//shader.SpecularPower = 0
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(headmesh)
-
-	facemesh := NewEmptyMesh()
-	//load face attempt
-	if headid == "0" {
-		facemesh, _ = LoadOBJ("Head.obj")
-	} else {
-		facemesh, _ = LoadOBJ("/var/www/html/itemcache/" + headid + ".obj")
-	}
-	allmeshes[2] = mesh
-	//mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	shader = NewPhongShader(matrix, light, eye)
-	if faceid == "0" {
-		facetexture, err := LoadTexture("face.png")
-		if err != nil {
-			panic(err)
-		}
-		shader.Texture = facetexture
-	} else {
-		facetexture, err := LoadTexture("/var/www/html/itemcache/" + faceid + ".png")
-		if err != nil {
-			panic(err)
-		}
-		shader.Texture = facetexture
-	}
-	//HexColor("#FEC400")
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(facemesh)
-
-	//load torso
-	torsomesh, _ := LoadOBJ("Torso.obj")
-	allmeshes[3] = mesh
-	//mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	shader = NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = HexColor(torsocolor)
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(torsomesh)
-
-	//load leftarm
-	leftarmmesh, _ := LoadOBJ("LeftArm.obj")
-	//mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	allmeshes[4] = mesh
-	shader = NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = HexColor(leftarmcolor)
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(leftarmmesh)
-
-	//load rightarm
-	rightarmmesh, _ := LoadOBJ("RightArm.obj")
-	allmeshes[5] = mesh
-	//mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	shader = NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = HexColor(rightarmcolor)
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(rightarmmesh)
-
-	if shirtid == "0" {
-		shirtid = "shirtStuds"
-	}
-	thisshatexture, err := LoadTexture("face" + ".png")
+	var namespace uuid.UUID
+	namespace, err = uuid.FromString(os.Getenv("THUMBNAIL_UUID_NAMESPACE"))
 	if err != nil {
-		panic(err)
-	}
-	//load torso
-
-	torsoshirtmesh, _ := LoadOBJ("Torso.obj")
-	allmeshes[6] = mesh
-	shader = NewPhongShader(matrix, light, eye)
-	shader.Texture = thisshatexture
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(torsoshirtmesh)
-
-	leftshirtmesh, _ := LoadOBJ("LeftArm.obj")
-	allmeshes[7] = mesh
-	shader = NewPhongShader(matrix, light, eye)
-	shader.Texture = thisshatexture
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(leftshirtmesh)
-
-	rightshirtmesh, _ := LoadOBJ("RightArm.obj")
-	allmeshes[8] = mesh
-	shader = NewPhongShader(matrix, light, eye)
-	shader.Texture = thisshatexture
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(rightshirtmesh)
-
-	/*mesh, _ = LoadOBJ("Shirt.obj")
-	shader = NewPhongShader(matrix, light, eye)
-	shader.Texture = thisshatexture
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(mesh)*/
-
-	tshirtmesh := NewEmptyMesh()
-	//load tshirt if it exists
-	if tshirtid != "0" {
-		tshirtmesh, _ = LoadOBJ("TShirt.obj")
-		allmeshes[9] = mesh
-		shader = NewPhongShader(matrix, light, eye)
-		tshtexture, err := LoadTexture("/var/www/html/itemcache/" + tshirtid + ".png")
-		if err != nil {
-			panic(err)
-		}
-		shader.Texture = tshtexture
-		//shader.ObjectColor = HexColor(torsocolor)
-		shader.DiffuseColor = Gray(0.25)
-		shader.SpecularColor = Gray(0.1)
-		shader.SpecularPower = 32
-		shader.AmbientColor = HexColor("#c1bfbf")
-		context.Shader = shader
-		context.DrawMesh(tshirtmesh)
+		return
 	}
 
-	//load leftleg
-	leftlegmesh, _ := LoadOBJ("LeftLeg.obj")
-	allmeshes[10] = mesh
-	//mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	shader = NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = HexColor(leftlegcolor)
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(leftlegmesh)
+	aspect := float64(e.Size) / float64(e.Size)
 
-	//load rightleg
-	rightlegmesh, _ := LoadOBJ("RightLeg.obj")
-	allmeshes[11] = mesh
-	//mesh.SmoothNormals()
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	shader = NewPhongShader(matrix, light, eye)
-	shader.ObjectColor = HexColor(rightlegcolor)
-	shader.DiffuseColor = Gray(0.25)
-	shader.SpecularColor = Gray(0.1)
-	shader.SpecularPower = 32
-	shader.AmbientColor = HexColor("#c1bfbf")
-	context.Shader = shader
-	context.DrawMesh(rightlegmesh)
+	matrix := fauxgl.LookAt(eye, center, up).Perspective(fovy, aspect, near, far)
+	shader := fauxgl.NewPhongShader(matrix, light, eye)
+	context := fauxgl.NewContext(e.Size, e.Size, scale, shader)
+	scene := fauxgl.NewScene(context)
 
-	//if hatsplit != null{
-	/*
-		for _, element := range hatsplit {
-			// element is the element from someSlice for where we are
-			if element != "0" {
-				mesh, _ = LoadOBJ("/var/www/html/itemcache/"+element+".obj")
-				allmeshes[len(allmeshes) + 1] = mesh
-				//mesh.SmoothNormals()
-				//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-				shader = NewPhongShader(matrix, light, eye)
-				thishatexture, err := LoadTexture("/var/www/html/itemcache/"+element+".png")
-				if err != nil {
-					panic(err)
-				}
-				shader.Texture = thishatexture
-				//shader.ObjectColor = HexColor(torsocolor)
-				shader.DiffuseColor = Gray(0.25)
-				shader.SpecularColor = Gray(0.1)
-				shader.SpecularPower = 32
-				shader.AmbientColor = HexColor("#c1bfbf")
-				context.Shader = shader
-				context.DrawMesh(mesh)
-			}
-		}
-	*/
-	//}
+	fileUUID := uuid.NewV5(namespace, e.AvatarJSON).String()
+	objFileName := fmt.Sprintf("./%s.obj", fileUUID)
+	pngFileName := fmt.Sprintf("./%s.png", fileUUID)
 
-	//matrix.LookAt(V(2, 5, 10), center, up)
-	//mesh.Transform(Scale(V(2.5, 2.5, 2.5)))
-	//.BoundingBox()
-
-	// downsample image for antialiasing
-	matrix = LookAt(eye, center, up).Perspective(fovy, aspect, near, far)
-
-	allMesh := NewEmptyMesh()
-	var boxes []Box
-	for _, v := range allmeshes {
-		if v == nil {
-			continue
-		}
-		allMesh.Add(v)
-		bb := v.BoundingBox()
-		boxes = append(boxes, bb)
+	cmd := exec.Command(
+		"/opt/bin/exporter/avatar-exporter",
+		"-s", "export_avatar_rs.gd",
+		"--json", e.AvatarJSON,
+		"--obj-path", objFileName,
+		"--png-path", pngFileName,
+	)
+	err = cmd.Run()
+	if err != nil {
+		return
 	}
-	box := BoxForBoxes(boxes)
-	b := NewCubeForBox(box)
-	b.BiUnitCube()
-	allMesh.FitInside(b.BoundingBox(), V(0.5, 0.5, 0.5))
 
-	indexed := 0
-	var addedFOV float64
-	for _, v := range allmeshes {
-		if v == nil {
-			continue
-		}
-		num := len(v.Triangles)
-		tris := allMesh.Triangles[indexed : num+indexed]
-		allInside := false
-		for !allInside && len(tris) > 0 {
-			for _, t := range tris {
-				v1 := shader.Vertex(t.V1)
-				v2 := shader.Vertex(t.V2)
-				v3 := shader.Vertex(t.V3)
+	tex, _ := fauxgl.LoadTexture(pngFileName)
+	mesh, _ := fauxgl.LoadOBJ(objFileName)
 
-				if v1.Outside() || v2.Outside() || v3.Outside() {
-					addedFOV += 5
-					matrix = LookAt(eye, center, up).Perspective(fovy+addedFOV, aspect, near, far)
-					shader.Matrix = matrix
-					allInside = false
-				} else {
-					allInside = true
-				}
-			}
-		}
+	scene.AddObject(&fauxgl.Object{
+		Texture: tex,
+		Mesh:    mesh,
+		Color:   fauxgl.HexColor("777"),
+	})
 
-		v = NewTriangleMesh(tris)
-		indexed += num
-	}
-	image := context.Image()
-	image = resize.Resize(width, height, image, resize.Bilinear)
+	shader.AmbientColor = fauxgl.HexColor("AAA")
+	shader.DiffuseColor = fauxgl.HexColor("777")
+	shader.SpecularPower = 0
 
-	// save image
-	SavePNG("goout.png", image)
+	newMatrix := scene.FitObjectsToScene(eye, center, up, fovy, aspect, near, far)
+	shader.Matrix = newMatrix
+	scene.Draw()
+
+	outImg := context.Image()
+	buf := new(bytes.Buffer)
+	png.Encode(buf, outImg)
+
+	// remove files to prevent tmp dir from filling
+	os.Remove(objFileName)
+	os.Remove(pngFileName)
+
+	resp = ImageResponse{UUID: uuid.NewV5(namespace, buf.String()).String(), Image: base64.StdEncoding.EncodeToString(buf.Bytes())}
+
+	return
 }
